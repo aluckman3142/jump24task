3f45e48a6a0c1269c5f9cfb59525f9d0c63553a9
aluckman3142 <adamluckman@yahoo.co.uk>
2022-04-04 12:53:01 +0100

    Jump24 Dev task api user import console command

---
+++ app/Console/Commands/StoreUsers.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class StoreUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'StoreUsers';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $start_page = 1;
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request('GET','https://reqres.in/api/users');
+        $data = json_decode($res->getBody()->getContents(), true);
+
+        $total_pages = $data['total_pages'];
+
+        for($i = $start_page; $i <= $total_pages; $i++){
+
+            $res = $client->request('GET','https://reqres.in/api/users?page='.$i);
+            $data = json_decode($res->getBody()->getContents(), true);
+
+            $users = $data['data'];
+
+            $this->saveUsers($users);
+        }
+    }
+
+    public function saveUsers($users){
+        foreach ($users as $user){
+            User::firstOrCreate([
+                'name' => $user['first_name'].' '.$user['last_name'],
+                'email' => $user['email'],
+                'password' => ' ',
+            ])->save();
+         }
+    }
+}
